9c5661962bc3c336e3cba3ee18789fac43b7da18
FredslundMagnus <s183905@student.dtu.dk>
2023-01-03 10:43:48 +0100

    d

--- S2/testpackage/src/data/make_dataset.py
+++ S2/testpackage/src/data/make_dataset.py
@@ -1,55 +1,80 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-import torch
 from os import listdir
 from os.path import join
+from pathlib import Path
+
+import click
 import numpy as np
+import torch
+from dotenv import find_dotenv, load_dotenv
+
 
 def onehot(v: int):
-    a = [0]*10
+    a = [0] * 10
     a[v] = 1
     return a
 
+
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-    
+    logger.info("making final data set from raw data")
+
     train: torch.Tensor = None
     train_label: torch.Tensor = None
     for file in listdir(input_filepath):
         if file == ".gitkeep":
             continue
-        data = np.load(join(input_filepath,file), allow_pickle=True)
+        data = np.load(join(input_filepath, file), allow_pickle=True)
         if file.startswith("train"):
             if train is None:
-                train = torch.as_tensor(data["images"].reshape((-1, 28*28)), dtype=float)
-                train_label = torch.as_tensor([onehot(v) for v in data["labels"]], dtype=float)
+                train = torch.as_tensor(
+                    data["images"].reshape((-1, 28 * 28)), dtype=float
+                )
+                train_label = torch.as_tensor(
+                    [onehot(v) for v in data["labels"]], dtype=float
+                )
             else:
-                train = torch.cat((train, torch.as_tensor(data["images"].reshape((-1, 28*28)), dtype=float)))
-                train_label = torch.cat((train_label, torch.as_tensor([onehot(v) for v in data["labels"]], dtype=float)))
+                train = torch.cat(
+                    (
+                        train,
+                        torch.as_tensor(
+                            data["images"].reshape((-1, 28 * 28)), dtype=float
+                        ),
+                    )
+                )
+                train_label = torch.cat(
+                    (
+                        train_label,
+                        torch.as_tensor(
+                            [onehot(v) for v in data["labels"]], dtype=float
+                        ),
+                    )
+                )
         else:
-            test: torch.Tensor = torch.as_tensor(data["images"].reshape((-1, 28*28)), dtype=float)
-            test_label: torch.Tensor = torch.as_tensor([onehot(v) for v in data["labels"]], dtype=float)
+            test: torch.Tensor = torch.as_tensor(
+                data["images"].reshape((-1, 28 * 28)), dtype=float
+            )
+            test_label: torch.Tensor = torch.as_tensor(
+                [onehot(v) for v in data["labels"]], dtype=float
+            )
 
-    train_std = (train-train.mean())/train.std()
-    test_std = (test-test.mean())/test.std()
+    train_std = (train - train.mean()) / train.std()
+    test_std = (test - test.mean()) / test.std()
     torch.save(train_std, join(output_filepath, "train_data.tensor"))
     torch.save(test_std, join(output_filepath, "test_data.tensor"))
     torch.save(train_label, join(output_filepath, "train_labels.tensor"))
     torch.save(test_label, join(output_filepath, "test_labels.tensor"))
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
--- S2/testpackage/src/models/train_model.py
+++ S2/testpackage/src/models/train_model.py
@@ -1,46 +1,63 @@
 import argparse
 import sys
-
-import torch
-import click
-
-from model import MyAwesomeModel
-from torch.utils.data import DataLoader
-from torch.optim import Adam
-from torch.nn import MSELoss
 from os.path import join
+
+import click
 import matplotlib.pyplot as plt
+import torch
+from model import MyAwesomeModel
+from torch.nn import MSELoss
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
 
 @click.group()
 def cli():
     pass
 
+
 def accuracy(model, data, labels):
     out = model(data)
-    predicted, actual = out.argmax(axis = 1), labels.argmax(axis = 1)
+    predicted, actual = out.argmax(axis=1), labels.argmax(axis=1)
     correct = predicted == actual
-    return float(sum(correct)/5000)
+    return float(sum(correct) / 5000)
 
-def load(train_data: str, train_label: str, test_data: str, test_label: str) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    return torch.load(join("data", "processed", train_data)), torch.load(join("data", "processed", train_label)), torch.load(join("data", "processed", test_data)), torch.load(join("data", "processed", test_label))
+
+def load(
+    train_data: str, train_label: str, test_data: str, test_label: str
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    return (
+        torch.load(join("data", "processed", train_data)),
+        torch.load(join("data", "processed", train_label)),
+        torch.load(join("data", "processed", test_data)),
+        torch.load(join("data", "processed", test_label)),
+    )
+
 
 @click.command()
-@click.option("--lr", default=1e-3, help='learning rate to use for training')
-@click.option("--epochs", default=5, help='number of epochs used for training')
+@click.option("--lr", default=1e-3, help="learning rate to use for training")
+@click.option("--epochs", default=5, help="number of epochs used for training")
 def train(lr, epochs):
     print("Training day and night")
     print(f"{lr = }")
     print(f"{epochs = }")
 
     model = MyAwesomeModel()
-    train_data, train_label, test_data, test_label = load("train_data.tensor", "train_labels.tensor", "test_data.tensor", "test_labels.tensor")
+    train_data, train_label, test_data, test_label = load(
+        "train_data.tensor",
+        "train_labels.tensor",
+        "test_data.tensor",
+        "test_labels.tensor",
+    )
     print(train_data.shape, train_label.shape)
     optimizer = Adam(model.parameters(), lr=lr)
     loss_fn = MSELoss()
-    train_loader = DataLoader(list(zip(train_data, train_label)), batch_size=100, shuffle=True)
-    
-    history = [0]*epochs
-    val = [0]*epochs
+    train_loader = DataLoader(
+        list(zip(train_data, train_label)), batch_size=100, shuffle=True
+    )
+
+    history = [0] * epochs
+    val = [0] * epochs
     for epoch in range(epochs):
         for data, label in train_loader:
             out = model(data)
@@ -55,15 +72,9 @@ def train(lr, epochs):
     plt.plot(history)
     plt.savefig(join("reports", "figures", "training_curve.png"))
 
-cli.add_command(train)
 
+cli.add_command(train)
 
 
 if __name__ == "__main__":
     cli()
-
-
-    
-    
-    
-    
